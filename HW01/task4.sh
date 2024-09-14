79ee399c048212bfc6f826d1322fe112f6750469
DhruvSinghi <dhruv.bittal.singhi@gmail.com>
2024-09-13 20:30:22 -0500

    Update task4.sh

--- HW01/task4.sh
+++ HW01/task4.sh
@@ -1,1 +1,8 @@
-
+#!/usr/bin/env zsh
+#SBATCH -p instruction
+#SBATCH -J FirstSlurm
+#SBATCH -o FirstSlurm-%j.out -e FirstSlurm-%j.err
+#SBATCH -c 2
+#SBATCH --time=0-00:01:00
+cd $SLURM_SUBMIT_DIR
+hostname
