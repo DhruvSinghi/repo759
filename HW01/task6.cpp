a921ba013b680ef28457a043866eb35fd865dd54
DhruvSinghi <dhruv.bittal.singhi@gmail.com>
2024-09-13 20:31:19 -0500

    Update task6.cpp

--- HW01/task6.cpp
+++ HW01/task6.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+using std::cout;
+int main(int argc, char** argv)
+{
+    int N = std::atoi(argv[1]);
+    for (int i = 0;i<=N;i++)
+    {
+        printf("%d ",i);
+    }
+    printf("\n");
+    for (int i = N;i>=0;i--)
+    {
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+}
