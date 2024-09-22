acb43ed5f6087d3c150425ddf4e61f90f0d5d5ab
DhruvSinghi <dhruv.bittal.singhi@gmail.com>
2024-09-23 05:12:40 +0530

    Add files via upload

---
+++ HW02/task1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <time.h>
+#include <chrono>
+#include "scan.h"
+using std::cout;
+int main(int argc, char** argv)
+{
+    long long int N = std::atoll(argv[1]);
+    double *arr = (double *)malloc((N+1)*sizeof(double));
+    srand((unsigned) time(NULL));
+    for(long long int i = 0; i < N; i++)
+    {
+      arr[i] =  -1 +   ((double)(rand())) /((double)(RAND_MAX/2));
+      cout << arr[i]<<std::endl;
+    }
+
+  auto start_time = std::chrono::high_resolution_clock::now();
+  scan(arr,N);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+  
+  cout<<duration.count()<<std::endl;
+  cout<<arr[0]<<std::endl;
+  cout<<arr[N-1]<<std::endl;
+
+
+}
