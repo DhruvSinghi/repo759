835f2d073b6e819298ce10140c99abf488d80599
DhruvSinghi <dhruv.bittal.singhi@gmail.com>
2024-09-23 14:25:56 -0500

    Update task.sh

--- HW02/task.sh
+++ HW02/task.sh
@@ -4,7 +4,7 @@
 #SBATCH -o secondSlurm-%j.out -e secondSlurm-%j.err
 #SBATCH -c 1
 #SBATCH --time=0-00:10:00
-cd $SLURM_SUBMIT_DIRECTORY
+cd $SLURM_SUBMIT_DIR
 module load gcc/13.2.0
 g++ scan.cpp task1.cpp-Wall-O3-std=c++17-o task1
 ./task1 1024
