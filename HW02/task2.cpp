c97ef7d6e8b1109ebdab24d1aa1b68edc98555eb
Dhruv <dhruv.bittal.singhi@gmail.com>
2024-09-29 14:11:24 -0500

    removed unnecessary print statements`

--- HW02/task2.cpp
+++ HW02/task2.cpp
@@ -50,7 +50,7 @@ for (i = 1;i<f.height-1;i++)
     f.pMatvals[i*f.height+f.height-1] = 1;
 }
 
-for(i = 0; i < f.height; i++)
+/*for(i = 0; i < f.height; i++)
 {
     for(j = 0; j < f.height; j++)
     {
@@ -58,7 +58,7 @@ for(i = 0; i < f.height; i++)
       if(j == f.height - 1)
       cout<<"\n";
     }
-}
+}*/
 
 for(i = 0; i < w.height; i++)
 {
@@ -67,7 +67,7 @@ for(i = 0; i < w.height; i++)
       w.pMatvals[i*w.height+j] = dist2(generator);
     }
 }
-for(i = 0; i < w.height; i++)
+/*for(i = 0; i < w.height; i++)
 {
     for(j = 0; j < w.height; j++)
     {
@@ -75,14 +75,14 @@ for(i = 0; i < w.height; i++)
       if(j == w.height - 1)
       cout<<"\n";
     }
-}
+}*/
 
 
 auto start_time = std::chrono::high_resolution_clock::now();
 convolve(f.pMatvals,g.pMatvals,n,w.pMatvals,m);
 auto end_time = std::chrono::high_resolution_clock::now();
 
-for(i = 0; i < g.height; i++)
+/*for(i = 0; i < g.height; i++)
 {
     for(j = 0; j < g.height; j++)
     {
@@ -90,12 +90,12 @@ for(i = 0; i < g.height; i++)
       if(j == g.height - 1)
       cout<<"\n";
     }
-}
+}*/
 duration <float, std::milli> duration_sec;
 duration_sec = std::chrono::duration_cast<duration<float, std::milli>>(end_time - start_time);
-cout<<"Time:"<<duration_sec.count()<<"ms"<<std::endl;
-cout<<"First element:"<<g.pMatvals[0]<<std::endl;
-cout<<"Last element:"<<g.pMatvals[(g.height-1)*g.height+g.height-1];
+cout<<duration_sec.count()<<std::endl;
+cout<<g.pMatvals[0]<<std::endl;
+cout<<g.pMatvals[(g.height-1)*g.height+g.height-1]<<std::endl;
 delete[] f.pMatvals;
 delete[] w.pMatvals;
 delete[] g.pMatvals;
