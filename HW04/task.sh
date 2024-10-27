c4058a14ac428a1235052872f414a1baa49166f7
Dhruv Bittal Singhi <singhi@euler-login-2.engr.wisc.edu>
2024-10-26 22:34:54 -0500

    Changes after Euler

--- HW04/task.sh
+++ HW04/task.sh
@@ -3,11 +3,15 @@
 #SBATCH -J nbody
 #SBATCH -o nbody-%j.out -e nbody-%j.err
 #SBATCH -c 8
-#SBATCH --time=0-00:50:00
+#SBATCH --time=0-00:10:00
 cd $SLURM_SUBMIT_DIR
 module load gcc/13.2.0
 g++ task3.cpp -Wall -O3 -std=c++17 -o task3 -fopenmp
 for (( i=1; i<=8; i++ ))
 do
-	    ./task3 400 10 $(i)
- done
+	        chunk_size=$(1)
+	        export OMP_SCHEDULE="guided,$chunk_size"
+		    echo "Running with OMP_SCHEDULE=$OMP_SCHEDULE for i=$i"
+		        ./task3 400 10 $i
+		done
+
