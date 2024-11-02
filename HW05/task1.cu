669ff8a3a0f4ccab29d0dfc612f9284dc63d2754
Dhruv Bittal Singhi <singhi@euler-login-1.engr.wisc.edu>
2024-11-02 09:08:07 -0500

    corrected task1

--- HW05/task1.cu
+++ HW05/task1.cu
@@ -4,16 +4,20 @@
 __global__ void kernel()
 {
     int b = 1;
-    for(int i = threadIDx.x + 1; i >= 1 ; i--)
+    for(int i = threadIdx.x + 1; i > 1; i--)
     {
-       b = b*i;
+	    b = b*i;
     }
-    printf("%d!=%d\n",threadIDx.x,b);
+    printf("%d!=%d\n",threadIdx.x+1,b);
     
 }
 int main()
 {
     kernel<<<1,8>>>();
     cudaDeviceSynchronize();
+    cudaError_t err = cudaGetLastError();
+    if(err != cudaSuccess) {
+	    fprintf(stderr,"Kernel Launch Failed %s\n",cudaGetErrorString(err));
+    }
     return 0;
 }
--- HW05/task1.sh
+++ HW05/task1.sh
@@ -2,12 +2,11 @@
 #SBATCH -p instruction
 #SBATCH -J fact
 #SBATCH -o fact-%j.out -e fact-%j.err
-#SBATCH -c 1
-#SBATCH --gpus-per-task = 1
-#SBATCH --time=0-00:01:00
+#SBATCH --gres=gpu:1 -c 1
+#SBATCH --time=0-00:10:00
 cd $SLURM_SUBMIT_DIR
-module load gcc/13.2.0
-module load nvidia/cuda
+module load gcc/11.3.0
+module load nvidia/cuda/12.0.0
 nvcc task1.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task1
 ./task1
 
