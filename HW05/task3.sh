ade3b362d3fb3dc88286f51b90958b1be6b8e483
Dhruv <dhruv.bittal.singhi@gmail.com>
2024-11-02 23:12:31 -0500

    changed task3.sh

--- HW05/task3.sh
+++ HW05/task3.sh
@@ -9,3 +9,7 @@ cd $SLURM_SUBMIT_DIR
 module load gcc/11.3.0
 module load nvidia/cuda/12.0.0
 nvcc task3.cu vscale.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o task3
+for (( i=10; i<=29; i++ ))
+do
+	    ./task3 $((2**i))
+ done
